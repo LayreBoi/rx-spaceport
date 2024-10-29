eb567c668fa80288508f6fa29b860792bc766202
Winter Stefan <stefan.winter@twinformatics.at>
2024-10-29 15:45:03 +0100

    ngrx fixed

--- src/app/store/app.selectors.ts
+++ src/app/store/app.selectors.ts
@@ -1,3 +1,6 @@
 import {AppState} from "./app.reducers";
+import {createSelector} from "@ngrx/store";
 
 export const selectAppState = (state: AppState) => state;
+
+export const selectStarted = createSelector(selectAppState, appState => appState.started);
